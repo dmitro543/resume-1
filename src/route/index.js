74aaea3fade3d6fe55211815d57b49c4ed22da5c
dmitro543 <123867495+dmitro543@users.noreply.github.com>
2023-10-10 20:17:34 +0300

    Update index.js

--- src/route/index.js
+++ src/route/index.js
@@ -1,169 +1,45 @@
-// Підключаємо технологію express для back-end сервера
-const express = require('express')
-// Cтворюємо роутер - місце, куди ми підключаємо ендпоїнти
-const router = express.Router()
+<!DOCTYPE html>
+<html>
+    <head>
+        <div>
+            {{#with js}}
+            <div>
+                <h1>{{name}}</h1>
 
-// ================================================================
+                {{#with data}}
+                <div>
+                   <p>name:{{name}}</p>
+                </div>
+                {{/with}} 
+                    <br>
 
-// router.get Створює нам один ентпоїнт
+                {{#each data}}
+                   <div>
+                     <p>description:{{description}}</p>
+                     <h2> history:{{history}}</h2>
+                     <h3> year: {{year}}</h3>
+                     <h4> founder: {{founder}}</h4>
+                   </div>
+                {{/each}}
+                    <br>
 
-router.get('/facebook', function (req, res) {
-  // res.render генерує нам HTML сторінку
+                 {{#each types}}
+                  <h5>
+                    types:{{types}}
+                  </h5>
+                 {{/each}}
+                    <br>
 
-  //            ↙ cюди вводимо назву файлу з сontainer
-  res.render('facebook', {
-    name: 'Facebook',
-    users: [
-      {
-        id: 1,
-        name: 'John Doe',
-        gender: 'Male',
-        age: 30,
-        friends: [
-          {
-            id: 2,
-            name: 'Jane Smith',
-            gender: 'Female',
-            age: 27,
-            mutual_friends: [
-              {
-                id: 4,
-                name: 'Samantha Lee',
-                gender: 'Female',
-                age: 25,
-              },
-            ],
-          },
-          {
-            id: 5,
-            name: 'Mike Williams',
-            gender: 'Male',
-            age: 28,
-            mutual_friends: [
-              {
-                id: 7,
-                name: 'David Kim',
-                gender: 'Male',
-                age: 29,
-              },
-            ],
-          },
-        ],
-        groups: [
-          {
-            id: 1,
-            name: 'Hiking Enthusiasts',
-            description:
-              'A group for people who love hiking and the great outdoors',
-            members: [
-              {
-                id: 8,
-                name: 'Sarah Johnson',
-                gender: 'Female',
-                age: 25,
-              },
-            ],
-          },
-          {
-            id: 2,
-            name: 'Foodies United',
-            description:
-              'A group for food lovers to share recipes and restaurant recommendations',
-            members: [
-              {
-                id: 5,
-                name: 'Mike Williams',
-                gender: 'Male',
-                age: 28,
-              },
-              {
-                id: 8,
-                name: 'Sarah Johnson',
-                gender: 'Female',
-                age: 25,
-              },
-            ],
-          },
-        ],
-      },
-      {
-        id: 16,
-        name: 'Amy Lee',
-        gender: 'Female',
-        age: 30,
-        friends: [
-          {
-            id: 15,
-            name: 'Peter Kim',
-            gender: 'Male',
-            age: 32,
-            mutual_friends: [
-              {
-                id: 17,
-                name: 'David Chen',
-                gender: 'Male',
-                age: 33,
-              },
-              {
-                id: 19,
-                name: 'Erica Wong',
-                gender: 'Female',
-                age: 29,
-                mutual_friends: [
-                  {
-                    id: 20,
-                    name: 'Alex Lee',
-                    gender: 'Male',
-                    age: 31,
-                  },
-                ],
-              },
-            ],
-            groups: [
-              {
-                id: 3,
-                name: 'Travel Addicts',
-                description:
-                  'A group for people who love to travel and explore new places',
-                members: [
-                  {
-                    id: 22,
-                    name: 'Jackie Chen',
-                    gender: 'Female',
-                    age: 26,
-                  },
-                ],
-              },
-              {
-                id: 4,
-                name: 'Pet Lovers',
-                description:
-                  'A group for people who love their furry friends',
-                members: [
-                  {
-                    id: 16,
-                    name: 'Amy Lee',
-                    gender: 'Female',
-                    age: 30,
-                  },
-                  {
-                    id: 25,
-                    name: 'John Smith',
-                    gender: 'Male',
-                    age: 33,
-                  },
-                ],
-              },
-            ],
-          },
-        ],
-      },
-    ],
-  })
-  //                  ↑↑ сюди вводимо JSON дані
-})
+                 {{#each santax}}
+                      <h6> variables:{{variables}} </h6>
+                      <p>  functions: {{functions}}</p>
+                      <p>  conditionals: {{conditionals}}</p>
+                     <span>loops: {{loops}}</span>
+                        <b>classes: {{classes}} </b>
+                 {{/each}}
 
-// ================================================================
-
-// Підключаємо роутер до бек-енду
-module.exports = router
+            </div>
+            {{/with}}
+        </div>
+    </head>
+</html>
